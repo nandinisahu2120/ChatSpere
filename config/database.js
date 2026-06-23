e033a18f2b83c1a88d9517db87f8868b1229e61b
nandinisahu2120 <nandinisahu2120@gmail.com>
2026-06-23 17:40:34 +0530

    feat: setup express server and database connection

---
+++ config/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log(`database connected broo  o`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+export default connectDB;
---
+++ index.js
@@ -0,0 +1,31 @@
+// const expr ess = require('express')//method 1 for importing express
+import express from "express"; //method 2
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import dns from "dns";
+
+//change dns
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+dotenv.config({});
+
+const app = express();
+
+// // Test route
+// app.get("/", (req, res) => {
+//   res.send("Backend is running (^.^)");
+// });
+
+const PORT = process.env.PORT || 5000;
+//middleware
+app.use(express.json());
+
+//routes
+app.use("/api/v1/user", userRoute);
+// http://localhost:8080/api/v1/user;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
+});
