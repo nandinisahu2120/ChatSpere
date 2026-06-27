ede4f6973c17a51a8f83b2f28e12b2c1261e19e4
nandinisahu2120 <nandinisahu2120@gmail.com>
2026-06-27 15:48:12 +0530

    feat: add send message controller and route

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,50 @@
+import { Conversation } from "../models/conversationModel.js";
+import { Message } from "../models/messageModel.js";
+
+export const sendMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+    const { message } = req.body;
+    let gotConversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!gotConversation) {
+      gotConversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+
+    const newMessage = await Message.create({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) {
+      gotConversation.messages.push(newMessage._id);
+    }
+    await gotConversation.save();
+
+    // Socket IO
+
+    return res.status(201).json({
+      message: "Message send successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getMessage = async (res, req) => {
+  try {
+    const receiverId = req.params.id;
+    const senderId = req.id;
+    const Conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+    console.log(Conversation);
+  } catch (error) {
+    console.log(error);
+  }
+};
