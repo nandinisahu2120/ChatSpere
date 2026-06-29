f4b08e20c9c67400edb827c0892b30ca8990e883
nandinisahu2120 <nandinisahu2120@gmail.com>
2026-06-29 16:08:37 +0530

    feat: add login and signup components

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:8080/api/v1/user/login",
+        user,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        },
+      );
+
+      navigate("/");
+      console.log(res);
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("Server not responding. Please try again.");
+      }
+    }
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-200  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-3xl font-bold text-center text-gray-900">Login</h1>
+        <form onSubmit={onSubmitHandler} action="">
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text text-gray-900">
+                Username
+              </span>
+            </label>
+            <input
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              type="text"
+              placeholder="Username"
+              className="text-gray-800 bg-gray-200 w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text text-gray-900">
+                Password
+              </span>
+            </label>
+            <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              type="Password"
+              placeholder="Password"
+              className="text-gray-800 bg-gray-200 w-full input input-bordered h-10"
+            />
+          </div>
+
+          <p className="text-center text-gray-900 my-2">
+            Don't have an account?{" "}
+            <Link className="text-blue-700" to="/register">
+              signup
+            </Link>
+          </p>
+
+          <div>
+            <button
+              type="submit"
+              className="btn btn-block btn-md  bg-gray-200 mt-2 border border-slate-400 text-gray-500"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
