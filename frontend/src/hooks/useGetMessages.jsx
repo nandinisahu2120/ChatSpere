212ef6db919bd5ee374ca32a70fbe347471dff79
nandinisahu2120 <nandinisahu2120@gmail.com>
2026-07-02 18:03:40 +0530

    Add socket connection hooks and redux slice for real-time updates

--- frontend/src/hooks/useGetMessages.jsx
+++ frontend/src/hooks/useGetMessages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setMessages } from "../redux/messageSlice";
@@ -6,20 +6,24 @@ import { setMessages } from "../redux/messageSlice";
 const useGetMessages = () => {
   const { selectedUser } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+
   useEffect(() => {
+    console.log("useEffect triggered, selectedUser:", selectedUser); // ADD THIS
     const fetchMessages = async () => {
       try {
+        console.log("Fetching messages for:", selectedUser?._id); // ADD THIS
         axios.defaults.withCredentials = true;
         const res = await axios.get(
           `http://localhost:8080/api/v1/message/${selectedUser?._id}`,
         );
-        // dispatch(setMessages(res.data));
+        console.log("Messages received:", res.data); // ADD THIS
         dispatch(setMessages(res.data || []));
       } catch (error) {
-        console.log(error);
+        console.log("Error fetching messages:", error); // ADD THIS (updated)
       }
     };
     fetchMessages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedUser]);
 };
 export default useGetMessages;
---
+++ frontend/src/hooks/useGetRealTimeMessage.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setMessages } from "../redux/messageSlice";
+
+const useGetRealTimeMessage = () => {
+  const { socket } = useSelector((store) => store.socket);
+  const { messages } = useSelector((store) => store.message);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleNewMessage = (newMessage) => {
+      dispatch(setMessages([...(messages || []), newMessage])); // ← null-safe fallback
+    };
+
+    socket.on("newMessage", handleNewMessage);
+
+    return () => socket.off("newMessage", handleNewMessage);
+  }, [socket, messages, dispatch]);
+};
+
+export default useGetRealTimeMessage;
