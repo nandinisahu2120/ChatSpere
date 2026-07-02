1c2bd1dc8657f79b01346272f723b855025c7fa9
nandinisahu2120 <nandinisahu2120@gmail.com>
2026-07-02 18:03:54 +0530

    Update message components for real-time rendering

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,35 +1,3 @@
-// import React, { useEffect, useRef } from "react";
-// import { useSelector } from "react-redux";
-
-// const Message = ({ message }) => {
-//   const scroll = useRef();
-//   const { authUser } = useSelector((store) => store.user);
-//   useEffect(() => {
-//     scroll.current?.scrollIntoView({ behavior: "smooth" });
-//   }, [message]);
-
-//   return (
-//     <div
-//       ref={scroll}
-//       className={`chat ${authUser?._id === message?.senderId ? "chat-end" : "chat-start"}`}
-//     >
-//       <div className="chat-image avatar">
-//         <div className="w-10 rounded-full">
-//           <img
-//             alt="Tailwind CSS chat bubble component"
-//             src="https://www.tensionends.com/wp-content/uploads/2022/09/Beautiful-Girl-DP-Images-21.jpg"
-//           />
-//         </div>
-//       </div>
-//       <div className="chat-header">
-//         <time className="text-xs opacity-50 text-gray-950">12:45</time>
-//       </div>
-//       <div className="chat-bubble">{message?.message}</div>
-//     </div>
-//   );
-// };
-// export default Message;
-
 import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 
@@ -62,7 +30,11 @@ const Message = ({ message }) => {
       <div className="chat-header">
         <time className="text-xs opacity-50 text-gray-950">12:45</time>
       </div>
-      <div className="chat-bubble">{message?.message}</div>
+      <div
+        className={`chat-bubble ${authUser?._id === message?.senderId ? "bg-gray-200 text-black " : "bg-gray-700 text-white "} `}
+      >
+        {message?.message}
+      </div>
     </div>
   );
 };
--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from "react";
+import React from "react";
 import SendInput from "./SendInput";
 import Messages from "./Messages";
-import { useDispatch, useSelector } from "react-redux";
-import { setSelectedUser } from "../redux/userSlice";
+// import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+// import { setSelectedUser } from "../redux/userSlice";
 
 const MessageContainer = () => {
-  const { selectedUser, authUser } = useSelector((store) => store.user);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    return () => dispatch(setSelectedUser(null));
-  }, []);
+  const { selectedUser, authUser, onlineUsers } = useSelector(
+    (store) => store.user,
+  );
+  const isOnline = onlineUsers?.includes(selectedUser?._id);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   return () => dispatch(setSelectedUser(null));
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   return (
     <>
       {selectedUser !== null ? (
         <div className="md:min-w-[550px] flex flex-col h-full">
           <div className="flex gap-2 items-center bg-gray-700 px-4 py-2 mb-2">
-            <div className="avatar online">
+            <div className={`avatar ${isOnline ? "online" : ""}`}>
               <div className="w-12 rounded-full">
-                <img src={selectedUser?.profilePhoto} alt="girl" />
+                <img src={selectedUser?.profilePhoto} alt="profile_photo" />
               </div>
             </div>
             <div className="flex flex-col flex-1">
